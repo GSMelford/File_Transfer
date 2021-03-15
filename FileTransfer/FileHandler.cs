3705996f8dd5fe7b835e3ee7aa1fb9420ad75ac3
Max Fedorenko <fedorenko.max163@gmail.com>
2021-03-15 17:39:04 +0200

    [Create] Класс FileHandler

---
+++ FileTransfer/FileHandler.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+
+namespace FileTransfer
+{
+    public class FileHandler
+    {
+        public delegate void FileHandlerDelegate(object sender, MyEventArgs e);
+
+        public static event FileHandlerDelegate Notify;
+        
+        private static List<string> FilePathsList;
+
+        private FileHandler()
+        {
+            FilePathsList = new List<string>();
+        }
+
+        public static void AddPaths(List<string> paths)
+        {
+            FilePathsList = paths;
+        }
+    }
+}
