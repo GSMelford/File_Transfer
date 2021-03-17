8866331d151132face70c718a15902cb4c9ab5eb
Max Fedorenko <fedorenko.max163@gmail.com>
2021-03-17 16:05:24 +0200

    [Create] Класс NetworkConnection
    Класс для работы с интернет соединением.
    Реализованы методы:
    Создание сервера.
    Принятие клиента.

---
+++ FileTransfer/NetworkConnection.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileTransfer
+{
+    class NetworkConnection
+    {
+        private string IP;
+        private static TcpListener Listener;
+        private static TcpClient Client;
+        private static NetworkStream Stream;
+        private NetworkConnection()
+        {
+            IP = new WebClient().DownloadString("https://api.ipify.org");
+        }
+
+        public static bool StartServer()
+        {
+            try
+            {
+                Listener = new TcpListener(IPAddress.Any, 1234);
+                Listener.Start();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public static bool AcceptClient()
+        {
+            try
+            {
+                Client = Listener.AcceptTcpClient();
+                Stream = Client.GetStream();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
