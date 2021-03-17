03aa97be43b6f9669684a1d893308a973913636c
Max Fedorenko <fedorenko.max163@gmail.com>
2021-03-17 16:47:55 +0200

    [Add] Класс для работы с файлами

---
+++ FileTransfer/FileHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileTransfer
+{
+    class FileHandler
+    {
+        private static string _downloadPath;
+        public static string DowloadPath 
+        {
+            get => _downloadPath;
+            set { _downloadPath = value; }
+        }
+
+        public static void WriteFile(byte[] file, string fileName)
+        {
+            using (FileStream fileStream = new FileStream($@"{_downloadPath}\{fileName}",FileMode.Append))
+            {
+                fileStream.Write(file, 0, file.Length);
+            }
+        }
+    }
+}
