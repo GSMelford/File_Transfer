1ac6989c1079826783e609cbad2127b3ca2852b8
Max Fedorenko <fedorenko.max163@gmail.com>
2021-03-17 16:49:58 +0200

    [Add] Методы для получения файла

--- FileTransfer/NetworkConnection.cs
+++ FileTransfer/NetworkConnection.cs
@@ -1,24 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace FileTransfer
 {
     class NetworkConnection
     {
-        private string IP;
+        private static string IP;
+        private static string IPServer = "178.150.32.105";
+        private static int Port;
         private static TcpListener Listener;
         private static TcpClient Client;
         private static NetworkStream Stream;
+
         private NetworkConnection()
         {
             IP = new WebClient().DownloadString("https://api.ipify.org");
         }
-
         public static bool StartServer()
         {
             try
@@ -38,11 +39,62 @@ namespace FileTransfer
             {
                 Client = Listener.AcceptTcpClient();
                 Stream = Client.GetStream();
+
+                //Запуск потока для получения файлов
+                Task receiveFiles = new Task(ReceiveFiles);
+                receiveFiles.Start();
+
                 return true;
             }
             catch (Exception)
             {
                 return false;
+            }
+        }
+        public static bool ConnectToServer(int  port)
+        {
+            try
+            {
+                Port = port;
+                Client = new TcpClient();
+                Client.Connect(IPServer, Port);
+                Stream = Client.GetStream();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        private static void ReceiveFiles()
+        {
+            while (true)
+            {
+                int bytes = 0;
+                byte[] buffer = new byte[64];
+                StringBuilder json = new StringBuilder();
+
+                do
+                {
+                    bytes = Stream.Read(buffer, 0, buffer.Length);
+                    json.Append(Encoding.UTF8.GetString(buffer, 0, bytes));
+                }
+                while (Stream.DataAvailable);
+
+                FileTransfer.FileInfo fileInfo = JsonSerializer.Deserialize<FileTransfer.FileInfo>(json.ToString());
+
+                int bufferSize = 10000; // Скорость отправки
+
+                for (int i = 0; i < fileInfo.Length; i+= bufferSize)
+                {
+                    if (fileInfo.Length - i - bufferSize < 0)
+                        bufferSize = fileInfo.Length - i;
+                    buffer = new byte[bufferSize];
+
+                    Stream.Read(buffer, 0, buffer.Length);
+
+
+                }
             }
         }
     }
