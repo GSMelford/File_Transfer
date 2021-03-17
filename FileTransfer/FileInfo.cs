1bdc44140946545473c72034558bdd22514f5d52
Max Fedorenko <fedorenko.max163@gmail.com>
2021-03-17 16:48:54 +0200

    [Add] Класс контейнер информации о файле

---
+++ FileTransfer/FileInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileTransfer
+{
+    class FileInfo
+    {
+        public string Name { get; set; }
+        public string Expansion { get; set; }
+        public int Length { get; set; }
+    }
+}
