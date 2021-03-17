f407799f23b479a24f8bc3949cd2f292774a187d
Max Fedorenko <fedorenko.max163@gmail.com>
2021-03-17 23:32:18 +0200

    [Create] Класс контейнер для событий
    Не использую, пока что.

---
+++ FileTransfer/FileTransferEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileTransfer
+{
+    public class FileTransferEventArgs
+    {
+        public string Message { get; set; }
+
+        public FileTransferEventArgs(string message)
+        {
+            Message = message;
+        }
+    }
+}
