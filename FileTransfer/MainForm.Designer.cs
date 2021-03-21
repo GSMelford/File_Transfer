29967f8da1192940838cc42c135a7c25007fc12d
Макс Федоренко <fedorenko.max163@gmail.com>
2021-03-22 01:01:33 +0200

    [Create] Новый дизайн формы

---
+++ FileTransfer/MainForm.Designer.cs
@@ -0,0 +1,244 @@
+﻿
+namespace FileTransfer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MenuPanel = new System.Windows.Forms.Panel();
+            this.VersionLabel = new System.Windows.Forms.Label();
+            this.DisconnectButton = new System.Windows.Forms.Button();
+            this.ClientButton = new System.Windows.Forms.Button();
+            this.HostButton = new System.Windows.Forms.Button();
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.TopTable = new System.Windows.Forms.TableLayoutPanel();
+            this.Table = new System.Windows.Forms.TableLayoutPanel();
+            this.ProgramName = new System.Windows.Forms.Label();
+            this.TopPanel = new System.Windows.Forms.Panel();
+            this.WindowLabel = new System.Windows.Forms.Label();
+            this.MenuPanel.SuspendLayout();
+            this.MainPanel.SuspendLayout();
+            this.TopPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MenuPanel
+            // 
+            this.MenuPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(17)))), ((int)(((byte)(23)))));
+            this.MenuPanel.Controls.Add(this.VersionLabel);
+            this.MenuPanel.Controls.Add(this.DisconnectButton);
+            this.MenuPanel.Controls.Add(this.ClientButton);
+            this.MenuPanel.Controls.Add(this.HostButton);
+            this.MenuPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.MenuPanel.Location = new System.Drawing.Point(0, 32);
+            this.MenuPanel.Name = "MenuPanel";
+            this.MenuPanel.Size = new System.Drawing.Size(157, 418);
+            this.MenuPanel.TabIndex = 1;
+            // 
+            // VersionLabel
+            // 
+            this.VersionLabel.BackColor = System.Drawing.Color.Transparent;
+            this.VersionLabel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.VersionLabel.Font = new System.Drawing.Font("Calibri", 8F, System.Drawing.FontStyle.Bold);
+            this.VersionLabel.ForeColor = System.Drawing.Color.White;
+            this.VersionLabel.Location = new System.Drawing.Point(0, 391);
+            this.VersionLabel.Name = "VersionLabel";
+            this.VersionLabel.Size = new System.Drawing.Size(157, 27);
+            this.VersionLabel.TabIndex = 7;
+            this.VersionLabel.Text = "Version 0.9.1";
+            this.VersionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // DisconnectButton
+            // 
+            this.DisconnectButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.DisconnectButton.FlatAppearance.BorderSize = 0;
+            this.DisconnectButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(50)))), ((int)(((byte)(64)))));
+            this.DisconnectButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.DisconnectButton.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Bold);
+            this.DisconnectButton.ForeColor = System.Drawing.Color.White;
+            this.DisconnectButton.Location = new System.Drawing.Point(0, 64);
+            this.DisconnectButton.Name = "DisconnectButton";
+            this.DisconnectButton.Size = new System.Drawing.Size(157, 32);
+            this.DisconnectButton.TabIndex = 6;
+            this.DisconnectButton.Text = "Disconnect";
+            this.DisconnectButton.UseVisualStyleBackColor = true;
+            this.DisconnectButton.Click += new System.EventHandler(this.DisconnectButton_Click);
+            // 
+            // ClientButton
+            // 
+            this.ClientButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ClientButton.FlatAppearance.BorderSize = 0;
+            this.ClientButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(50)))), ((int)(((byte)(64)))));
+            this.ClientButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ClientButton.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Bold);
+            this.ClientButton.ForeColor = System.Drawing.Color.White;
+            this.ClientButton.Location = new System.Drawing.Point(0, 32);
+            this.ClientButton.Name = "ClientButton";
+            this.ClientButton.Size = new System.Drawing.Size(157, 32);
+            this.ClientButton.TabIndex = 5;
+            this.ClientButton.Text = "Client";
+            this.ClientButton.UseVisualStyleBackColor = true;
+            this.ClientButton.Click += new System.EventHandler(this.ClientButton_Click);
+            // 
+            // HostButton
+            // 
+            this.HostButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.HostButton.FlatAppearance.BorderSize = 0;
+            this.HostButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(50)))), ((int)(((byte)(64)))));
+            this.HostButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.HostButton.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Bold);
+            this.HostButton.ForeColor = System.Drawing.Color.White;
+            this.HostButton.Location = new System.Drawing.Point(0, 0);
+            this.HostButton.Name = "HostButton";
+            this.HostButton.Size = new System.Drawing.Size(157, 32);
+            this.HostButton.TabIndex = 4;
+            this.HostButton.Text = "Host";
+            this.HostButton.UseVisualStyleBackColor = true;
+            this.HostButton.Click += new System.EventHandler(this.HostButton_Click);
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.Controls.Add(this.Table);
+            this.MainPanel.Controls.Add(this.TopTable);
+            this.MainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainPanel.Location = new System.Drawing.Point(157, 32);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(723, 418);
+            this.MainPanel.TabIndex = 2;
+            // 
+            // TopTable
+            // 
+            this.TopTable.AutoScroll = true;
+            this.TopTable.ColumnCount = 7;
+            this.TopTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.TopTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.TopTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.TopTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.TopTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.TopTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.TopTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.TopTable.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopTable.Location = new System.Drawing.Point(0, 0);
+            this.TopTable.Name = "TopTable";
+            this.TopTable.RowCount = 1;
+            this.TopTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.TopTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.TopTable.Size = new System.Drawing.Size(723, 32);
+            this.TopTable.TabIndex = 0;
+            // 
+            // Table
+            // 
+            this.Table.AutoScroll = true;
+            this.Table.ColumnCount = 7;
+            this.Table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.Table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.Table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.Table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.Table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.Table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.Table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.Table.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Table.Location = new System.Drawing.Point(0, 32);
+            this.Table.Name = "Table";
+            this.Table.RowCount = 1;
+            this.Table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.Table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 356F));
+            this.Table.Size = new System.Drawing.Size(723, 386);
+            this.Table.TabIndex = 1;
+            // 
+            // ProgramName
+            // 
+            this.ProgramName.BackColor = System.Drawing.Color.Transparent;
+            this.ProgramName.Dock = System.Windows.Forms.DockStyle.Left;
+            this.ProgramName.Font = new System.Drawing.Font("Calibri", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ProgramName.ForeColor = System.Drawing.Color.White;
+            this.ProgramName.Location = new System.Drawing.Point(0, 0);
+            this.ProgramName.Name = "ProgramName";
+            this.ProgramName.Size = new System.Drawing.Size(157, 32);
+            this.ProgramName.TabIndex = 1;
+            this.ProgramName.Text = "Menu";
+            this.ProgramName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.ProgramName.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ProgramName_MouseDown);
+            this.ProgramName.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ProgramName_MouseMove);
+            // 
+            // TopPanel
+            // 
+            this.TopPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(27)))), ((int)(((byte)(34)))));
+            this.TopPanel.Controls.Add(this.WindowLabel);
+            this.TopPanel.Controls.Add(this.ProgramName);
+            this.TopPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopPanel.Location = new System.Drawing.Point(0, 0);
+            this.TopPanel.Name = "TopPanel";
+            this.TopPanel.Size = new System.Drawing.Size(880, 32);
+            this.TopPanel.TabIndex = 0;
+            this.TopPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TopPanel_MouseDown);
+            this.TopPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.TopPanel_MouseMove);
+            // 
+            // WindowLabel
+            // 
+            this.WindowLabel.BackColor = System.Drawing.Color.Transparent;
+            this.WindowLabel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.WindowLabel.Font = new System.Drawing.Font("Calibri", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WindowLabel.ForeColor = System.Drawing.Color.White;
+            this.WindowLabel.Location = new System.Drawing.Point(157, 0);
+            this.WindowLabel.Name = "WindowLabel";
+            this.WindowLabel.Size = new System.Drawing.Size(157, 32);
+            this.WindowLabel.TabIndex = 2;
+            this.WindowLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(12)))), ((int)(((byte)(16)))));
+            this.ClientSize = new System.Drawing.Size(880, 450);
+            this.Controls.Add(this.MainPanel);
+            this.Controls.Add(this.MenuPanel);
+            this.Controls.Add(this.TopPanel);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MainForm";
+            this.MenuPanel.ResumeLayout(false);
+            this.MainPanel.ResumeLayout(false);
+            this.TopPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel MenuPanel;
+        private System.Windows.Forms.Button HostButton;
+        private System.Windows.Forms.Button ClientButton;
+        private System.Windows.Forms.Label VersionLabel;
+        private System.Windows.Forms.Button DisconnectButton;
+        private System.Windows.Forms.Panel MainPanel;
+        private System.Windows.Forms.TableLayoutPanel TopTable;
+        private System.Windows.Forms.TableLayoutPanel Table;
+        private System.Windows.Forms.Label ProgramName;
+        private System.Windows.Forms.Panel TopPanel;
+        private System.Windows.Forms.Label WindowLabel;
+    }
+}
---
+++ FileTransfer/MainForm.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileTransfer
+{
+    public partial class MainForm : Form
+    {
+        //Number //File Name //ProgressBar //Precent //Speed //Size //Time
+        private Point mousePoint;
+        public MainForm()
+        {
+            InitializeComponent();
+
+            Table.Visible = false;
+            TopTable.Visible = false;
+        }
+
+        private void HostButton_Click(object sender, EventArgs e)
+        {
+            MainPanel.Controls.Clear();
+            WindowLabel.Text = "Ctreate host:";
+
+            Label label1 = new Label();
+            label1.Text = "Attention! To create a server you must have:" +
+                "\n1) Static IP Address." +
+                "\n2) The port passed through the router(if you use it)." +
+                "\n3) Your computer must be connected to the network via a cable." +
+                "\n\nIf all these rules are sub-ice, enter the port to connect.";
+            label1.Dock = DockStyle.Bottom;
+            label1.TextAlign = ContentAlignment.MiddleLeft;
+            label1.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            label1.AutoSize = true;
+            label1.ForeColor = Color.Red;
+
+            Label label2 = new Label();
+            label2.Text = "Here is your IP address:";
+            label2.Location = new Point(10, 10);
+            label2.TextAlign = ContentAlignment.MiddleLeft;
+            label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            label2.AutoSize = true;
+            label2.ForeColor = Color.White;
+
+            TextBox ipAdressBox = new TextBox();
+            ipAdressBox.ReadOnly = true;
+            ipAdressBox.Location = new Point(20, 40);
+            ipAdressBox.Text = new WebClient().DownloadString("https://api.ipify.org");
+            ipAdressBox.Size = new Size(154, 32);
+            ipAdressBox.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            ipAdressBox.TextAlign = HorizontalAlignment.Center;
+            ipAdressBox.MaxLength = 39;
+
+            Label label3 = new Label();
+            label3.Text = "Enter the port:";
+            label3.Location = new Point(10, 74);
+            label3.TextAlign = ContentAlignment.MiddleLeft;
+            label3.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            label3.AutoSize = true;
+            label3.ForeColor = Color.White;
+
+            TextBox portBox = new TextBox();
+            portBox.Location = new Point(20, 102);
+            portBox.Size = new Size(154, 32);
+            portBox.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            portBox.TextAlign = HorizontalAlignment.Center;
+            portBox.MaxLength = 6;
+
+            Button button = new Button();
+            button.Location = new Point(19, 148);
+            button.Size = new Size(154, 48);
+            button.BackColor = Color.FromArgb(22, 27, 34);
+            button.FlatAppearance.BorderSize = 0;
+            button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(50)))), ((int)(((byte)(64)))));
+            button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            button.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Bold);
+            button.ForeColor = System.Drawing.Color.White;
+            button.Name = "CreateButton";
+            button.Size = new System.Drawing.Size(157, 32);
+            button.TabIndex = 6;
+            button.Text = "Create";
+            button.UseVisualStyleBackColor = true;
+
+            this.AcceptButton = button;
+
+            MainPanel.Controls.AddRange(new Control[] { button,portBox, label3, ipAdressBox, label2, label1 });
+
+
+
+        }
+
+        private void ClientButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DisconnectButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //Управление панелью 
+        private void TopPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            mousePoint = new Point(e.X, e.Y);
+        }
+        private void TopPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - mousePoint.X;
+                this.Top += e.Y - mousePoint.Y;
+            }
+        }
+        private void ProgramName_MouseDown(object sender, MouseEventArgs e)
+        {
+            mousePoint = new Point(e.X, e.Y);
+        }
+        private void ProgramName_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - mousePoint.X;
+                this.Top += e.Y - mousePoint.Y;
+            }
+        }
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        
+    }
+}
